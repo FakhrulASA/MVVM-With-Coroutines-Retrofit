2efaf8bd5f2246a942b202c2ad1127ae6f36fe72
fakhrul <fakhrul.alam@btracsl.com>
2021-04-10 18:05:03 +0600

    - Coroutines added

--- app/src/main/java/com/example/databindingmvvm/PostViewModel.kt
+++ app/src/main/java/com/example/databindingmvvm/PostViewModel.kt
@@ -2,17 +2,22 @@ package com.example.databindingmvvm
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class PostViewModel():ViewModel(){
     var postData:MutableLiveData<PostData> = MutableLiveData()
+    private lateinit var job: Job
     fun getPost(){
         var repository:Repository= Repository(Api())
         GlobalScope.launch(Dispatchers.Main){
          postData.value= repository.getMovies()
         }
+        job=Coroutines.ioThenMain(
+            {repository.getMovies()},
+            {
+                postData.value=it
+            }
+        )
     }
 
 }
