0e43917c5aa81793d17367371bfbf1cc363dbba1
fakhrul <fakhrul.alam@btracsl.com>
2021-04-18 12:15:16 +0600

    -recyclerview added

--- app/src/main/java/com/example/databindingmvvm/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/databindingmvvm/adapter/PostAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.databindingmvvm.adapter
 
-class PostAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.databindingmvvm.R
+import com.example.databindingmvvm.data.PostData
+import com.example.databindingmvvm.data.PostDataItem
+import com.example.databindingmvvm.databinding.ItemPostBinding
+
+class PostAdapter(
+    private val posts:List<PostDataItem>
+): RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+
+    inner class PostViewHolder(
+        val itemPostBinding: ItemPostBinding
+    ):RecyclerView.ViewHolder(itemPostBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=PostViewHolder(
+        DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_post,
+            parent,
+            false
+        )
+    )
+
+    override fun getItemCount(): Int {
+       return posts.size
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.itemPostBinding.posts=posts[position]
+    }
 }
--- app/src/main/java/com/example/databindingmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/databindingmvvm/ui/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.example.databindingmvvm.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.example.databindingmvvm.network.Api
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.databindingmvvm.R
+import com.example.databindingmvvm.adapter.PostAdapter
+import com.example.databindingmvvm.network.Api
 import com.example.databindingmvvm.repository.Repository
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val repository =
             Repository(Api())
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val model: PostViewModel by viewModels()
 
         model.getPost()
-            model.postData.observe(
-                this@MainActivity,
-                Observer {
+        model.postData.observe(
+            this@MainActivity,
+            Observer { posts ->
+                recyclerView.also {
+                    it.layoutManager = LinearLayoutManager(this)
+                    it.setHasFixedSize(true)
+                    it.adapter = PostAdapter(posts)
+                }
 
-                    Log.d("HALA", it.get(2).title)
-                })
+            })
     }
 }
